5d5952e03473a0d2509fb03e629f325ba3ca1dd4
e_Yuan_Access <541578753@qq.com>
2024-06-07 10:40:14 +0800

    add wgan method, Splitting the network into encoder and decoder, add recon loss

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 from dataset import get_dataloader, get_img_shape
-from network import GeneratorResNet,  Discriminator
+from network import GeneratorResNet,  Discriminator,  wgan_flag
 from utils import *
 
 
@@ -16,13 +16,19 @@ def train(genG:GeneratorResNet, genF:GeneratorResNet, disG:Discriminator, disF:D
     
     n_epochs = 5000
     batch_size = 1
-    lr = 2e-4
+    lr = 1e-4
     beta1 = 0.5
-    k_max = 5
-
-    criterion_GAN = torch.nn.MSELoss().to(device)
-    criterion_Cycle = torch.nn.L1Loss().to(device)
-    criterion_Identity = torch.nn.L1Loss().to(device)
+    if not wgan_flag:
+        k_max = 6
+    else:
+        k_max = 1
+    print("wgan_flag=", wgan_flag)
+    # criterion_GAN = torch.nn.MSELoss().to(device)
+    criterion_GAN = torch.nn.BCELoss().to(device)
+    criterion_Cycle = torch.nn.MSELoss().to(device)
+    criterion_Identity = torch.nn.MSELoss().to(device)
+    criterion_Latent = torch.nn.L1Loss().to(device)
+    criterion_Recon = torch.nn.MSELoss().to(device)
 
     Imgx_dir_name = '../face2face/dataset/train/C'
     Imgy_dir_name = '../face2face/dataset/train/A'
@@ -38,29 +44,40 @@ def train(genG:GeneratorResNet, genF:GeneratorResNet, disG:Discriminator, disF:D
 
     gen_params = list(genG.parameters()) + list(genF.parameters())
     dis_params = list(disG.parameters()) + list(disF.parameters())
-    optim_gen = torch.optim.Adam([p for p in gen_params if p.requires_grad],
-                                            lr, betas=(beta1, 0.999), weight_decay=0.0001)# betas 是一个优化参数，简单来说就是考虑近期的梯度信息的程度
-    optim_dis = torch.optim.Adam([p for p in dis_params if p.requires_grad],
-                                            0.5*lr, betas=(beta1, 0.999), weight_decay=0.0001)
 
+    dis_weight = 0.5
+    if not wgan_flag:
+        optim_gen = torch.optim.Adam([p for p in gen_params if p.requires_grad],
+                                            lr, betas=(beta1, 0.999), weight_decay=0.0001)# betas 是一个优化参数，简单来说就是考虑近期的梯度信息的程度
+        optim_dis = torch.optim.Adam([p for p in dis_params if p.requires_grad],
+                                 dis_weight*lr, betas=(beta1, 0.999), weight_decay=0.0001)
+    else:
+        optim_gen = torch.optim.RMSprop([p for p in gen_params if p.requires_grad],           lr) #
+        optim_dis = torch.optim.RMSprop([p for p in dis_params if p.requires_grad],dis_weight*lr) #WGAN作者建议不要用Adam
+    
     genG = genG.train()
     genF = genF.train()
     disG = disG.train()
     disF = disF.train()
-    label_fake = torch.full((batch_size,1,16,16), 0., dtype=torch.float, device=device)       # 真实图是1，虚假是0,需要注意,这里用的时候是计算loss，是小数，要用1. 和0.
-    label_real = torch.full((batch_size,1,16,16), 1., dtype=torch.float, device=device)       # 还有一件事，这里不能随意用batch_size，因为最后可能不满512，但是还是会继续算。得实时读取x的bn大小
+    label_fake = torch.full((batch_size,1,3,3), 0.01, dtype=torch.float, device=device).detach()       # 真实图是1，虚假是0,需要注意,这里用的时候是计算loss，是小数，要用1. 和0.
+    label_real = torch.full((batch_size,1,3,3), 0.99, dtype=torch.float, device=device).detach()       # 还有一件事，这里不能随意用batch_size，因为最后可能不满512，但是还是会继续算。得实时读取x的bn大小
                                                                     # 一般四个维度分别是bn c h w
                                                                     # 我把他从循环中挪出来并且检测x的bn不是batch_size就跳过
     xy_buffer = ReplayBuffer()
     yx_buffer = ReplayBuffer()
 
-    k = k_max
-    for epoch_i in tqdm(range(n_epochs)):
+    k = 0
+    for epoch_i in range(n_epochs):
         tic = time.time()
-        if(epoch_i%100==0 and k < k_max):          # 让k缓慢的增大到k_max
+        if(epoch_i%50==0 and k < k_max):          # 让k缓慢的增大到k_max
             k+=1
         datax_iter = iter(dataloaderx)
         datay_iter = iter(dataloadery)
+
+
+        loss_list = torch.zeros((11,1)).to(device)
+        loss_list = loss_list.squeeze()
+
         for _ in tqdm(range(len_dataset)):
             x,_  = next(datax_iter)
             y,_  = next(datay_iter)
@@ -69,47 +86,115 @@ def train(genG:GeneratorResNet, genF:GeneratorResNet, disG:Discriminator, disF:D
 
             if(x.shape[0]!=batch_size):
                 continue
-            
+
             # 训练Gen
+            
+            for param in disG.parameters():
+                param.requires_grad = False
+            for param in disF.parameters():
+                param.requires_grad = False
+            
 
-            xy = genG(x)
-            yx = genF(y)
-            g_loss_gan = criterion_GAN(disG(xy), label_real) + criterion_GAN(disF(yx), label_real)
+            xGe = genG.encode(x)
+            xy = genG.decode(xGe)
+            yFe = genF.encode(y)
+            yx = genF.decode(yFe)
+            xGeFdx = genF.decode(xGe)
+            yFeGdy = genG.decode(yFe)
 
-            xyx = genF(xy)
-            yxy = genG(yx)
-            g_loss_cycle = criterion_Cycle(xyx, x) + criterion_Cycle(yxy, y)
+            g_loss_recon = criterion_Recon(x,xGeFdx) + criterion_Recon(y, yFeGdy)
 
+            if not wgan_flag:
+                g_loss_gan = criterion_GAN(disG(xy), label_real) + criterion_GAN(disF(yx), label_real)               # woc xdm训练gen时千万记得别再傻呵呵用fake label当target了
+                # g_loss_gan_G = criterion_GAN(disG(xy), label_real)
+                # g_loss_gan_F = criterion_GAN(disF(yx), label_real)
+            else:
+                g_wloss_G = -torch.mean(disG(xy))
+                g_wloss_F = -torch.mean(disF(yx))
+                g_loss_gan = g_wloss_G+g_wloss_F
+                
+
+            xyFe = genF.encode(xy)
+            xyx = genF.decode(xyFe)
+            yxGe = genG.encode(yx)
+            yxy = genG.decode(yxGe)
+
+            g_loss_latent = criterion_Latent(xGe, xyFe) + criterion_Latent(yFe, yxGe)
+            
+
+            
+
+            g_loss_cycle_G = criterion_Cycle(xyx, x)
+            g_loss_cycle_F = criterion_Cycle(yxy, y) 
+            g_loss_cycle = g_loss_cycle_G + g_loss_cycle_F
+            # print(g_loss_cycle_1, g_loss_cycle_2)
             xx = genF(x)
             yy = genG(y)
             g_loss_identity = criterion_Identity(xx, x) + criterion_Identity(yy, y)
-
-            g_loss = g_loss_gan * 100 + g_loss_cycle*10 + g_loss_identity*5
+            # g_loss_identity_G = criterion_Identity(xx, x)
+            # g_loss_identity_F = criterion_Identity(yy, y)
+            g_loss = g_loss_gan*10+ g_loss_cycle*20 + g_loss_identity*0 + g_loss_latent*1 + g_loss_recon*40
+            # g_loss_G = g_loss_gan_G * gan_const + g_loss_cycle_G*20 + g_loss_identity_G*10
+            # g_loss_F = g_loss_gan_F * gan_const + g_loss_cycle_F*20 + g_loss_identity_F*10
+            # g_loss = g_loss_G + g_loss_F + g_loss_latent
             optim_gen.zero_grad()
             g_loss.backward()
             optim_gen.step()
 
             # 训练Dis
-            xy_ = xy_buffer.push_and_pop(xy).to(device)
-            yx_ = yx_buffer.push_and_pop(yx).to(device)
-            d_loss_G_real = criterion_GAN(disG(y), label_real)
-            d_loss_G_fake = criterion_GAN(disG(xy_), label_fake)
-            d_loss_F_real = criterion_GAN(disF(x), label_real)
-            d_loss_F_fake = criterion_GAN(disF(yx_), label_fake)
+            if(epoch_i%k==0):
+                for param in disG.parameters():
+                    param.requires_grad = True
+                for param in disF.parameters():
+                    param.requires_grad = True
+                optim_dis.zero_grad()
 
-            d_loss = d_loss_G_real + d_loss_G_fake + d_loss_F_real + d_loss_F_fake
-            optim_dis.zero_grad()
-            d_loss.backward()
-            optim_dis.step()
+                xy_ = xy_buffer.push_and_pop(xy).to(device).detach()
+                yx_ = yx_buffer.push_and_pop(yx).to(device).detach()
+                dgr = disG(y)
+                dgf = disG(xy_)
+                dfr = disF(x)
+                dff = disF(yx_)
+                # print(dgr, dgf ,dfr ,dff)
+                
+                if not wgan_flag:
+                    d_loss_G_real = criterion_GAN(dgr, label_real)
+                    d_loss_G_fake = criterion_GAN(dgf, label_fake)
+                    d_loss_F_real = criterion_GAN(dfr, label_real)
+                    d_loss_F_fake = criterion_GAN(dff, label_fake)
+                    d_loss = (d_loss_G_real + d_loss_G_fake + d_loss_F_real + d_loss_F_fake)  
+                else:
+                    d_wloss_G = -torch.mean(dgr-dgf) 
+                    d_wloss_F = -torch.mean(dfr-dff) 
+                    d_loss = (d_wloss_G + d_wloss_F)
+                
+
+                d_loss.backward()
+                optim_dis.step()
+
+                if not wgan_flag:
+                    pass
+                else:
+                    for p in disG.parameters():
+                        p.data.clamp_(-0.01, 0.01)
+                    for p in disF.parameters():
+                        p.data.clamp_(-0.01, 0.01)
+            loss_list += torch.stack([g_loss, d_loss, g_loss_gan, g_loss_cycle, g_loss_identity, g_loss_latent, g_loss_recon, d_loss_G_real, d_loss_G_fake, d_loss_F_real, d_loss_F_fake])  #把列表转换成tensor
             
-            
+
+
+
+        loss_list = loss_list/len_dataset    
         toc = time.time()    
         gan_weights = {'genG': genG.state_dict(), 'genF': genF.state_dict(), 'disG': disG.state_dict(), 'disF': disF.state_dict()}
         torch.save(gan_weights, ckpt_path)
-        print(f'epoch {epoch_i} g_loss: {g_loss.item():.4e} d_loss: {d_loss.item():.4e} time: {(toc - tic):.2f}s')
-        print(f'g_loss_gan {g_loss_gan.item():.4e} g_loss_cycle: {10*g_loss_cycle.item():.4e} g_loss_identity: {5*g_loss_identity.item():.4e} ')
-        print(f'd_loss_G_real {d_loss_G_real.item():.4e} d_loss_G_fake: {d_loss_G_fake.item():.4e} d_loss_F_real: {d_loss_F_real.item():.4e} d_loss_F_fake: {d_loss_F_fake.item():.4e}')
-        if(epoch_i%10==0):
+        print(f'epoch {epoch_i} g_loss: {loss_list[0].item():.4e} d_loss: {loss_list[1].item():.4e} time: {(toc - tic):.2f}s')
+        print(f'g_loss_gan {loss_list[2].item():.4e} g_loss_cycle: {loss_list[3].item():.4e} g_loss_identity: {loss_list[4].item():.4e} g_loss_latent: {loss_list[5].item():.4e} g_loss_recon: {loss_list[6].item():.4e}')
+        if not wgan_flag:
+            print(f'd_loss_G_real {loss_list[7].item():.4e} d_loss_G_fake: {loss_list[8].item():.4e} d_loss_F_real: {loss_list[9].item():.4e} d_loss_F_fake: {loss_list[10].item():.4e}')
+        else:
+            print(f'g_wloss_G {g_wloss_G.item():.4e} g_wloss_F {g_wloss_F.item():.4e} d_wloss_G {d_wloss_G.item():.4e} d_wloss_F: {d_wloss_F.item():.4e}')
+        if(epoch_i%1==0):
             sample(genG, genF, device=device)
 
 sample_time = 0
@@ -125,19 +210,31 @@ def sample(genG:GeneratorResNet, genF:GeneratorResNet, device='cuda'):
     genF = genF.eval()
     with torch.no_grad():
         Imgx_dir_name = '../face2face/dataset/train/C'
-        dataloaderx = get_dataloader(i_n, Imgx_dir_name)
+        Imgy_dir_name = '../face2face/dataset/train/A'
+        dataloaderx = get_dataloader(i_n,Imgx_dir_name)
+        dataloadery = get_dataloader(i_n,Imgy_dir_name)
         x,_  = next(iter(dataloaderx))
+        y,_  = next(iter(dataloadery))
         x = x.to(device)
         xy = genG(x)
         xyx = genF(xy)
-        x_stack = torch.stack((x, xy, xyx), dim = 0)
+        xx = genF(x)
+        y = y.to(device)
+        yx = genF(y)
+        yxy = genG(yx)
+        yy = genG(y)
+        xGe = genG.encode(x)
+        yFe = genF.encode(y)
+        xGeFdx = genF.decode(xGe)
+        yFeGdy = genG.decode(yFe)
+        x_stack = torch.stack((x, xy, xyx, xx, xGeFdx, y, yx, yxy, yy, yFeGdy), dim = 0)
         x_new = einops.rearrange(x_stack, 'x n c h w -> (x h) (n w) c')
         x_new = (x_new.clip(-1, 1) + 1) / 2 * 255
         x_new = x_new.cpu().numpy().astype(np.uint8)
         # print(x_new.shape)
         if(x_new.shape[2]==3):
             x_new = cv2.cvtColor(x_new, cv2.COLOR_RGB2BGR)
-        cv2.imwrite('cyclegan_sample_%d.jpg' % (sample_time), x_new)
+        cv2.imwrite(os.path.join(save_dir,'cyclegan_sample_%d.jpg' % (sample_time)), x_new)
     genG = genG.train()
     genF = genF.train()
 
@@ -152,21 +249,28 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)     ## m.weight.data表示需要初始化的权重. nn.init.normal_():表示随机初始化采用正态分布，均值为0，标准差为0.02.
         torch.nn.init.constant_(m.bias.data, 0.0)           ## nn.init.constant_():表示将偏差定义为常量0.
 
+save_dir = './data/face2anime_wgan64/'
 
 if __name__ == '__main__':
 
-    ckpt_path = './model_cyclegan.pth'
+    ckpt_path = os.path.join(save_dir,'model_cyclegan.pth')
     device = 'cuda'
     image_shape = get_img_shape()
-    genG = GeneratorResNet(image_shape, 9).to(device)    # x->y
-    genF = GeneratorResNet(image_shape, 9).to(device)    # y->x
+    genG = GeneratorResNet(image_shape, 11).to(device)    # x->y
+    genF = GeneratorResNet(image_shape, 11).to(device)    # y->x
     disG = Discriminator(image_shape).to(device)
     disF = Discriminator(image_shape).to(device)
     genG.apply(weights_init_normal)
     genF.apply(weights_init_normal)
     disG.apply(weights_init_normal)
     disF.apply(weights_init_normal)
+    # gan_weights = torch.load(ckpt_path)
+    # genG.load_state_dict(gan_weights['genG'])
+    # genF.load_state_dict(gan_weights['genF'])
+    # disG.load_state_dict(gan_weights['disG'])
+    # disF.load_state_dict(gan_weights['disF'])
     # sample(genG, genF, device=device)
+
     train(genG, genF, disG, disF, ckpt_path, device=device)
     gan_weights = torch.load(ckpt_path)
     genG.load_state_dict(gan_weights['genG'])
--- network.py
+++ network.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import numpy as np
+wgan_flag = False
 
 ## 定义参数初始化函数
 def weights_init_normal(m):                                    
@@ -12,7 +14,10 @@ def weights_init_normal(m):
     elif classname.find("BatchNorm2d") != -1:               ## find():实现查找classname中是否含有BatchNorm2d字符，没有返回-1；有返回0.
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)     ## m.weight.data表示需要初始化的权重. nn.init.normal_():表示随机初始化采用正态分布，均值为0，标准差为0.02.
         torch.nn.init.constant_(m.bias.data, 0.0)           ## nn.init.constant_():表示将偏差定义为常量0.
-
+    elif classname.find("Linear") != -1:  # 添加线性层的初始化
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, "bias") and m.bias is not None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
 
 ##############################
 ##  残差块儿ResidualBlock
@@ -46,8 +51,8 @@ class GeneratorResNet(nn.Module):
         channels = input_shape[0]                           ## 输入通道数channels = 3
 
         ## 初始化网络结构
-        out_features = 64                                   ## 输出特征数out_features = 64 
-        model = [                                           ## model = [Pad + Conv + Norm + ReLU]
+        out_features = 128                                  ## 输出特征数out_features = 64 
+        encoder = [                                           ## model = [Pad + Conv + Norm + ReLU]
             nn.ReflectionPad2d(channels),                   ## ReflectionPad2d(3):利用输入边界的反射来填充输入张量
             nn.Conv2d(channels, out_features, 7),           ## Conv2d(3, 64, 7)
             nn.InstanceNorm2d(out_features),                ## InstanceNorm2d(64):在图像像素上对HW做归一化，用在风格化迁移
@@ -58,21 +63,27 @@ class GeneratorResNet(nn.Module):
         ## 下采样，循环2次
         for _ in range(2):
             out_features *= 2                                                   ## out_features = 128 -> 256
-            model += [                                                          ## (Conv + Norm + ReLU) * 2
+            encoder += [                                                          ## (Conv + Norm + ReLU) * 2
                 nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
                 nn.InstanceNorm2d(out_features),
                 nn.ReLU(inplace=True),
             ]
             in_features = out_features                                          ## in_features = 256
 
+        num_encoder_blocks = np.int_(np.floor(num_residual_blocks/2))
         # 残差块儿，循环9次
-        for _ in range(num_residual_blocks):
-            model += [ResidualBlock(out_features)]                              ## model += [pad + conv + norm + relu + pad + conv + norm]
+        for _ in range(num_encoder_blocks):
+            encoder += [ResidualBlock(out_features)]                              ## model += [pad + conv + norm + relu + pad + conv + norm]
+
+        decoder = []
+        num_decoder_blocks = num_residual_blocks-num_residual_blocks
+        for _ in range(num_decoder_blocks):
+            decoder += [ResidualBlock(out_features)]
 
         # 上采样两次
         for _ in range(2):
             out_features //= 2                                                  ## out_features = 128 -> 64
-            model += [                                                          ## model += [Upsample + conv + norm + relu]
+            decoder += [                                                          ## model += [Upsample + conv + norm + relu]
                 nn.Upsample(scale_factor=2),
                 nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
                 nn.InstanceNorm2d(out_features),
@@ -81,12 +92,22 @@ class GeneratorResNet(nn.Module):
             in_features = out_features                                          ## out_features = 64
 
         ## 网络输出层                                                            ## model += [pad + conv + tanh]
-        model += [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]    ## 将(3)的数据每一个都映射到[-1, 1]之间
+        decoder+= [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]    ## 将(3)的数据每一个都映射到[-1, 1]之间
 
-        self.model = nn.Sequential(*model)
+        self.encoder = nn.Sequential(*encoder)
+        self.decoder = nn.Sequential(*decoder)
 
     def forward(self, x):           ## 输入(1, 3, 256, 256)
-        return self.model(x)        ## 输出(1, 3, 256, 256)
+        x = self.encoder(x)
+        return self.decoder(x)        ## 输出(1, 3, 256, 256)
+    
+    def encode(self, x):
+        return self.encoder(x)
+    
+    def decode(self, x):
+        return self.decoder(x)
+    
+
 
 
 
@@ -116,11 +137,21 @@ class Discriminator(nn.Module):
             *discriminator_block(128, 256),                             ## layer += [conv(128, 256) + norm + relu]
             *discriminator_block(256, 512),                             ## layer += [conv(256, 512) + norm + relu]
             nn.ZeroPad2d((1, 0, 1, 0)),                                 ## layer += [pad]
+            nn.Conv2d(512, 512, 4, padding=1),                          ##
             nn.Conv2d(512, 1, 4, padding=1)                             ## layer += [conv(512, 1)]
         )
 
+        if not wgan_flag:
+            self.sig = nn.Sequential(
+                    nn.Sigmoid()                                        # sigmoid既有非线性作用，值域还是0-1，很适合当分数
+            )
+        else:
+            self.sig = nn.Sequential(
+                    nn.Identity()
+            )
     def forward(self, img):             ## 输入(1, 3, 256, 256) 
-        return self.model(img)          ## 输出(1, 1, 16, 16)
+        out = self.model(img)          ## 输出(1, 1, 16, 16)
+        return self.sig(out)
 
 
 
